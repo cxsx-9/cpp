a2265e501842c1b38d9427975f6ce84817d7a08c
cxsx-9 <99903983+cxsx-9@users.noreply.github.com>
2023-09-12 12:49:30 +0700

    .

--- module02/ex02/Fixed.hpp
+++ module02/ex02/Fixed.hpp
@@ -14,14 +14,42 @@ class Fixed
 		Fixed( const int num );
 		Fixed( const float num );
 		Fixed( const Fixed &copy );
-		Fixed &operator=(const Fixed &copy);
 		~Fixed( void );
 		int		getRawBits( void ) const;
 		void	setRawBits( int const raw );
 		float	toFloat( void ) const;
 		int		toInt( void ) const;
+
+		Fixed	&operator=( const Fixed &copy );
+
+		Fixed	&operator++(void);  // pre increment
+		Fixed	&operator--(void);
+		Fixed	operator++( int num ); // post increment
+		Fixed	operator--( int num );
+
+		// bool
+		bool	operator>( const Fixed &copy ) const;
+		bool	operator<( const Fixed &copy ) const;
+		bool	operator>=( const Fixed &copy ) const;
+		bool	operator<=( const Fixed &copy ) const;
+		bool	operator==( const Fixed &copy ) const;
+		bool	operator!=( const Fixed &copy ) const;
+
+		// math
+		Fixed	operator+( const Fixed &copy ) const;
+		Fixed	operator-( const Fixed &copy ) const;
+		Fixed	operator*( const Fixed &copy ) const;
+		Fixed	operator/( const Fixed &copy ) const;
+
+		//min max
+		static const Fixed	&min( const Fixed &copy1, const Fixed &copy2 );
+		static const Fixed	&max( const Fixed &copy1, const Fixed &copy2 );
 };
 
-std::ostream	&operator<<(std::ostream &out, Fixed const &fixed);
+std::ostream	&operator<<( std::ostream &out, Fixed const &fixed );
+//  >, <, >=, <=, == and !=		done
+// +, -, *, and /				done
+// The 4 increment/decrement
+// min max
 
 #endif
--- module02/ex02/main.cpp
+++ module02/ex02/main.cpp
@@ -5,14 +5,21 @@ int main( void ) {
     Fixed a;
     Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 
+    std::cout << "a		:";
     std::cout << a << std::endl;
+    std::cout << "++a		:";
     std::cout << ++a << std::endl;
+    std::cout << "a		:";
     std::cout << a << std::endl;
+    std::cout << "a++		:";
     std::cout << a++ << std::endl;
+    std::cout << "a		:";
     std::cout << a << std::endl;
 
+    std::cout << "b		:";
     std::cout << b << std::endl;
 
+    std::cout << "find max	:";
     std::cout << Fixed::max( a, b ) << std::endl;
 
     return 0;
