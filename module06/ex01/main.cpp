cf0e48118a436ec13d943e03cf6f3d2f18d28b2e
cxsx-9 <99903983+cxsx-9@users.noreply.github.com>
2023-10-31 15:16:51 +0700

    .

--- module06/ex01/main.cpp
+++ module06/ex01/main.cpp
@@ -1,6 +1,5 @@
 #include "Serializer.hpp"
 
-
 void	displayInformation(Data *cus)
 {
 	std::cout << GREEN;
@@ -20,18 +19,24 @@ int main()
 	Data	*dummy = Serializer::deserialize(ptr1);
 	
 	displayInformation(dummy);
+	std::cout << "	:from dummy" << std::endl;
 	
 	std::cout << YELLOW << "\nChange the value inside Data" << DEFAULT << std::endl;
+	
 	customer.id = 1;
-	
 	displayInformation(&customer);
+	std::cout << "	:from customer" << std::endl;
 	
 	std::cout << YELLOW << "\nChange the value inside Data" << DEFAULT << std::endl;
-	customer.name = "Phillip";
 	
+	dummy->name = "Phillip";
 	displayInformation(dummy);
+	std::cout << "	:from dummy" << std::endl;
 
+
+
+	std::cout << std::endl;
 	std::cout << "Address of customer :	" << &customer << std::endl;
-	std::cout << "Address of ptr1 :	" << &ptr1 << std::endl;
 	std::cout << "Address of dummy :	" << dummy << std::endl;
+	std::cout << std::endl;
 }
